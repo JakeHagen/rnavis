3d0da368afe36314b853bd89d2d4d65e4380b7ab
Jake Hagen <jake.hagen@opmbx.org>
2016-04-24 17:46:41 -0400

    seperate data processing to seperate batch and normal processing from main page

--- app.py
+++ app.py
@@ -6,51 +6,48 @@ import json
 import config
 import pandas
 import numpy as np
-from sklearn.decomposition import PCA as sklearnPCA
+
 import tmm
 import ge
+import combat
 
 app = Flask(__name__)
 engine = sqly.create_engine(config.psql)
 meta = sqly.MetaData()
 meta.reflect(engine)
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/')
 def index():
-    errors = []
-    results = []
-    sample_names = []
     all_tables = list(meta.tables)
-    explained_variance = []
     counts_tables = [x for x in all_tables if "pipeline" not in x]
+    return render_template('index.html', counts_tables = counts_tables)                            #sample_names = sample_names, #explained_variance = explained_variance)
 
+@app.route('/data', methods = ['GET', 'POST'])
+def data():
+    table = request.form.get("postgres_table", "don_scott_high_fat_gene_counts")
+    rna_exp = ge.gene_expression(pandas.read_sql_table(table, index_col = 'gene',
+                                    con = engine))
+    results = ge.pca_json(rna_exp)
+    return json.dumps(results)
 
-    if request.method == "POST":
-        try:
-            table = request.form['table']
-            rna_exp = ge.gene_expression(pandas.read_sql_table(table, index_col = 'gene', con = engine))
-            mat_normed = ge.voom_out(rna_exp)
-            sklearn_pca = sklearnPCA(n_components = 4)
-            pca_points = sklearn_pca.fit_transform(mat_normed.T)
-            explained_variance = sklearn_pca.explained_variance_ratio_
-            #explained_variance = [x for x in explained_variance if x >+ .05]
-            print(explained_variance)
-            #pca_points = pca_points[:,0:len(explained_variance)]
-            print(pca_points)
-            #pca_points_df = pandas.DataFrame(pca_points, columns = ['pc%s' % x for x in range(len(explained_variance))])
-            pca_points_df = pandas.DataFrame(pca_points, columns = ['pc1', 'pc2', 'pc3', 'pc4'])
-            pca_points_df['sample'] = rna_exp.samples
-            results = pca_points_df.to_dict(orient = 'records')
-
-
-        except:
-            errors.append(
-                "Unable to get URL. PLease make sure it's valid and try again."
-            )
-    return render_template('index.html', results = json.dumps(results),
-                            sample_names = sample_names, counts_tables = counts_tables,
-                            explained_variance = explained_variance)
-
+@app.route('/batch', methods = ['GET', 'POST'])
+def batch():
+    batch_list = request.args.get(['batch'])
+    #        print(batch_list)
+    #        mat_combat = combat.combat(mat_normed, batch = [1,1,1,1,2,2,2,1,1,1,1,2,2,2],
+    #            model = np.array([[1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1],
+    #                            [1,0], [1,0], [1,0], [1,0], [1,0], [1,0], [1,0]]))
+    #        sklearn_pca_combat = sklearnPCA(n_components = 4)
+    #        pca_points_combat = sklearn_pca_combat.fit_transform(mat_combat.T)
+    #        pca_points_df_combat = pandas.DataFrame(pca_points_combat, columns = ['pc1', 'pc2', 'pc3', 'pc4'])
+    #        pca_points_df_combat['sample'] = rna_exp.samples
+    #        print(pca_points_df_combat)
+    #        results2 = pca_points_df_combat.to_dict(orient = 'records')
+    data = json.loads(request.form['bat'])
+    l = list(data)
+    print(l)
+    #return json.dumps(request.form['userName'])
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
