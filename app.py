b4ca62a338127b1dbc6399aec3322b9294b372be
Jake Hagen <jake.hagen@opmbx.org>
2016-04-27 14:33:54 -0400

    flask receives batch list and table as json, parses correctly and returns combat normalized pca points

--- app.py
+++ app.py
@@ -19,35 +19,34 @@ meta.reflect(engine)
 @app.route('/')
 def index():
     all_tables = list(meta.tables)
-    counts_tables = [x for x in all_tables if "pipeline" not in x]
+    counts_tables = [x for x in all_tables if "voom" not in x]
     return render_template('index.html', counts_tables = counts_tables)
 
 @app.route('/data', methods = ['GET', 'POST'])
 def data():
-    table = request.form.get("postgres_table", "don_scott_high_fat_gene_counts")
-    rna_exp = ge.gene_expression(pandas.read_sql_table(table, index_col = 'gene',
-                                    con = engine))
-    results = ge.pca_json(rna_exp)
+    table = request.form.get("postgres_table", None)
+    try:
+        rna_exp = ge.gene_expression(pandas.read_sql_table(table + "_voom",
+                                        index_col = 'gene', con = engine))
+        results = ge.pca_json(rna_exp, voom = False)
+    except:
+        rna_exp = ge.gene_expression(pandas.read_sql_table(table,
+                                        index_col = 'gene', con = engine))
+        voom_df = ge.voom_out(rna_exp)
+        voom_df.to_sql(table + "_voom", con = engine)
+        results = ge.pca_json(rna_exp, voom = True)
     return json.dumps(results)
 
 @app.route('/batch', methods = ['GET', 'POST'])
 def batch():
-    batch_list = request.args.get(['batch'])
-    print(batch_list)
-    #        mat_combat = combat.combat(mat_normed, batch = [1,1,1,1,2,2,2,1,1,1,1,2,2,2],
-    #            model = np.array([[1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1],
-    #                            [1,0], [1,0], [1,0], [1,0], [1,0], [1,0], [1,0]]))
-    #        sklearn_pca_combat = sklearnPCA(n_components = 4)
-    #        pca_points_combat = sklearn_pca_combat.fit_transform(mat_combat.T)
-    #        pca_points_df_combat = pandas.DataFrame(pca_points_combat, columns = ['pc1', 'pc2', 'pc3', 'pc4'])
-    #        pca_points_df_combat['sample'] = rna_exp.samples
-    #        print(pca_points_df_combat)
-    #        results2 = pca_points_df_combat.to_dict(orient = 'records')
-    data = json.loads(request.form['bat'])
-    l = list(data)
-    print(l)
-    #return json.dumps(request.form['userName'])
-    return render_template('test.html')
+    j = request.get_json('j')
+    table = j['table']
+    rna_exp = ge.gene_expression(pandas.read_sql_table(table + "_voom", index_col = 'gene',
+                                    con = engine))
+    batch_list = list(j['batch'])
+    rna_exp_batch = ge.gene_expression(combat.combat(rna_exp.df, batch = batch_list))
+    combat_results = ge.pca_json(rna_exp_batch, voom = False)
+    return json.dumps(combat_results)
 
 if __name__ == '__main__':
     app.run(debug = True)
