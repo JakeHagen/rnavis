613ceaaca1321e80164d4532542b6f4339d5b9f8
Jake Hagen <jake.hagen@opmbx.org>
2016-04-27 14:35:23 -0400

    added function to produce pca points with explained variance, ready to be converted to json

--- ge.py
+++ ge.py
@@ -1,5 +1,7 @@
 import tmm
 import numpy as np
+import pandas
+from sklearn.decomposition import PCA as sklearnPCA
 
 class gene_expression:
 
@@ -16,13 +18,26 @@ class gene_expression:
         self.number_of_groups = len(set(
             [''.join([i for i in x if not i.isdigit()]) for x in self.samples]
                                         ))
-
-
 def voom_out(ge):
-    '''Takes gene_expression class, will eventually extend it to just take a matrix
-    '''
-    mat_cpm = ge.matrix / (ge.lib_size / 1e6)
-    mat = ge.matrix[(mat_cpm > 1).sum(axis = 1) >= int(len(ge.samples) / ge.number_of_groups)]
+    mat_cpm = ge.df / (ge.lib_size / 1e6)
+    mat = ge.df[(mat_cpm > 1).sum(axis = 1) >= int(len(ge.samples) / ge.number_of_groups)]
     norm_factors = (ge.lib_size * tmm.calc_norm_factors(mat)) + 1
     mat_five = mat + .5
     return np.log2(mat_five / norm_factors * 1e6)
+
+
+def pca_json(ge, voom = True):
+    if voom:
+        mat_normed = voom_out(ge)
+    else:
+        mat_normed = ge.matrix
+    sklearn_pca = sklearnPCA(n_components = 4)
+    pca_points = sklearn_pca.fit_transform(mat_normed.T)
+    explained_variance = sklearn_pca.explained_variance_ratio_
+    exp_var = [x for x in explained_variance if x >= .0001]
+    pca_points_df = pandas.DataFrame(pca_points[:,0:len(exp_var)],
+                    columns = ["pc%s" % str(x+1) for x in range(len(exp_var))])
+    pca_points_df['sample'] = ge.samples
+    for x in range(len(explained_variance)):
+        pca_points_df['exp_var%s' % str(x+1)] = explained_variance[x]
+    return pca_points_df.to_dict(orient = 'records')
