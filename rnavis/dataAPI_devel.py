4bc6251f8b2146dfd3ba3bcea5a1a881d39558ef
Jake Hagen <jake.hagen@opmbx.org>
2016-05-23 12:24:50 -0400

    change standalone api to use matrix_manipulation class

--- rnavis/dataAPI_devel.py
+++ rnavis/dataAPI_devel.py
@@ -1,25 +1,23 @@
 from rnavis import app
 from flask_restful import Resource, Api, reqparse
 import pandas
-import rnavis.gene_expression as ge
-import rnavis.combat as combat
 
 
 api = Api(app)
 
 get_parser = reqparse.RequestParser()
-get_parser.add_argument('org_matrix', dest='org_matrix', required=False,
-                        location='headers',
+get_parser.add_argument('counts_matrix_name', dest='counts_matrix_name',
+                        required=False, location='headers',
                         help='Get the original matrix, must be posted first')
-get_parser.add_argument('norm_matrix', dest='norm_matrix', required=False,
-                        location='headers',
+get_parser.add_argument('norm_matrix_name', dest='norm_matrix_name',
+                        required=False, location='headers',
                         help='Get the normalized matrix')
-get_parser.add_argument('batch_matrix', dest='batch_matrix', required=False,
-                        location='headers',
+get_parser.add_argument('batch_matrix_name', dest='batch_matrix_name',
+                        required=False, location='headers',
                         help='Get normalized and batch corrected matrix')
 
 post_parser = reqparse.RequestParser()
-post_parser.add_argument('org_matrix_name', dest='org_matrix_name',
+post_parser.add_argument('counts_matrix_name', dest='counts_matrix_name',
                          required=False, location='json',
                          help='Name of the original matrix')
 post_parser.add_argument('norm_matrix_name', dest='norm_matrix_name',
@@ -47,30 +45,29 @@ put_parser.add_argument('name_matrix_to_correct',
 class pca_points(Resource):
 
     def __init__(self):
-        self.org_dict = {}
-        self.norm_dict = {}
-        self.batch_dict = {}
+        self.mm()  # mm for matrix_manipulation
 
     def get(self):
         args = get_parser.parse_args()
-        if sum([args.org_matrix, args.norm_matrix, args.batch_matrix]) > 1:
+        if sum([args.counts_matrix, args.norm_matrix, args.batch_matrix]) > 1:
             return {'status': 501, 'message': 'can only return one matrix'}
-        if sum([args.org_matrix, args.norm_matrix, args.batch_matrix]) == 0:
+        if sum([args.counts_matrix, args.norm_matrix, args.batch_matrix]) == 0:
             return {'status': 400, 'message': 'need to specify matrix'}
-        if args.org_matrix:
-            return self.org_dict[args.org_matrix]
-        if args.norm_matrix:
+        if args.counts_matrix_name:
+            return self.mm.counts_dict[args.counts_matrix_name]
+        if args.norm_matrix_name:
             try:
-                return self.norm_dict[args.norm_matrix]
+                return self.mm.norm_dict[args.norm_matrix_name]
             except KeyError:
                 try:
-                    return ge.norm_gene_matrix(self.org_dict[args.norm_matrix])
+                    self.mm.norm_counts_matrix(args.norm_matrix_name)
+                    return self.mm.norm_dict[args.norm_matrix_name]
                 except KeyError:
                     return {'status': 404,
                             'message': 'need to post matrix first'}
         if args.batch_matrix:
             try:
-                return self.batch_dict[args.batch_matrix]
+                return self.mm.batch_dict[args.batch_matrix]
             except KeyError:
                 return {'status': 404,
                         'message': 'matrix needs to be batch corrected first'}
@@ -84,21 +81,20 @@ class pca_points(Resource):
                 args.batch_matrix_name]) > 1:
             return {'status': 501,
                     'message': 'can add one type of matrix per time'}
-        if args.batch_matrix_name:
-            self.batch_dict[args.batch_matrix_name] = pandas.read_json(
-                                                    args.matrix, numpy=True
-                                                                    )
-            return self.batch_dict[args.batch_matrix_name]
+        # if args.batch_matrix_name:
+        #     self.mm.batch_dict[args.batch_matrix_name] = pandas.read_json(
+        #         args.matrix, numpy=True)
+        #     return self.batch_dict[args.batch_matrix_name]
         if args.norm_matrix_name:
-            self.norm_dict[args.norm_matrix_name] = pandas.read_json(
-                                                    args.matrix, numpy=True
-                                                                    )
-            return self.norm_dict[args.norm_matrix_name]
-        if args.org_matrix_name:
-            self.org_dict[args.org_matrix_name] = pandas.read_json(
-                                                    args.matrix, numpy=True
-                                                                    )
-            return self.norm_dict[args.org_matrix_name]
+            norm_matrix = pandas.read_json(args.matrix, numpy=True)
+            self.mm.add_norm_matrix(name=args.norm_matrix_name,
+                                    norm_matrix=norm_matrix)
+            return self.mm.norm_dict[args.norm_matrix_name]
+        if args.counts_matrix_name:
+            counts_matrix = pandas.read_json(args.matrix, numpy=True)
+            self.mm.add_counts_matrix(name=args.counts_matrix_name,
+                                      counts_matrix=counts_matrix)
+            return self.mm.counts_dict[args.counts_matrix]
         return {'status': 400, 'message': 'something was wrong with request'}
 
     def put(self):
@@ -110,14 +106,13 @@ class pca_points(Resource):
             return {'status': 400,
                     'message': 'cant batch correct two matrices at once'}
         try:
-            matrix_to_correct = self.norm_dict[args.name_matrix_to_correct]
-            self.batch_dict[args.name_matrix_to_correct] = combat.combat(
-                                matrix_to_correct, batch=list(args.batch_list))
-            return self.batch_dict[args.name_matrix_to_correct]
-        except KeyError:
+            self.mm.make_batch_matrix(batch_list=args.batch_list,
+                                      name=args.name_matrix_to_correct)
+            return self.mm.batch_dict[args.name_matrix_to_correct]
+        except:  # Need to catch the specific error
             matrix_to_correct = pandas.read_json(args.matrix_to_correct)
-            return combat.combat(matrix_to_correct,
-                                 batch=list(args.batch_list))
+            return self.mm.make_batch_matrix(batch_list=args.batch_list,
+                                             matrix=matrix_to_correct)
         return {'status': 400, 'message': 'something was wrong with request'}
 
 api.add_resource(pca_points, '/data')
