8526797cd532f492997ec98a85dd0e7903a5f262
Jake Hagen <jake.hagen@opmbx.org>
2016-05-23 12:23:59 -0400

    make changes to use new matrix_manipulation class: code is more readable and the class allows caching. No need to constantly read from database

--- rnavis/dataAPI.py
+++ rnavis/dataAPI.py
@@ -2,11 +2,9 @@ from rnavis import app
 from flask_restful import Resource, Api, reqparse
 import pandas
 import rnavis.gene_expression as ge
-import rnavis.combat as combat
 import sqlalchemy as sql
 import rnavis.config as config
 import psycopg2  # needs to imported because sqlalchemy uses it by default
-
 
 api = Api(app)
 
@@ -22,46 +20,33 @@ post_parser.add_argument('batch', dest='batch', required=False,
 
 engine = sql.create_engine(config.psql)
 
+mm = ge.matrix_manipulation()
+
 
 class pca_points(Resource):
+
     def post(self):
         args = post_parser.parse_args()
-        print(args)
+
         if args.batch:
             try:
-                # rna_exp = ge.gene_expression(
-                rna_exp = pandas.read_sql_table(args.table + "_voom",
-                                                index_col='Gene',
-                                                con=engine,
-                                                schema=args.schema)  # )
-                # rna_exp_batch = ge.gene_expression(
-                rna_exp_batch = combat.combat(rna_exp,
-                                              batch=list(args.batch))  # )
-                results = ge.pca_json(rna_exp_batch)  # , voom=False)
-                results = results.to_dict(orient='records')
-            except:   # Need to catch error psycopg2.ProgrammingError
+                matrix_name = args.schema + '_' + args.table
+                mm.make_batch_matrix(batch_list=list(args.batch),
+                                     name=matrix_name)
+                results = ge.pca_json(mm.get_batch_matrix(name=matrix_name))
+            except KeyError:
                 print("need voom normalized matrix before batch correcting")
         else:
             try:
-                # rna_exp = ge.gene_expression(
-                rna_exp = pandas.read_sql_table(args.table + "_voom",
-                                                index_col='Gene',
-                                                con=engine,
-                                                schema=args.schema)  # )
-                results = ge.pca_json(rna_exp)  # , voom=False)
-                results = results.to_dict(orient='records')
-            except:
-                # rna_exp = ge.gene_expression(
-                rna_exp = pandas.read_sql_table(args.table,
-                                                index_col='Gene',
-                                                con=engine,
-                                                schema=args.schema)  # )
-                norm_df = ge.norm_gene_matrix(rna_exp)
-                # print("writing to db")
-                # voom_df.to_sql(args.table + "_voom",
-                #                con=engine, schema=args.schema)
-                results = ge.pca_json(norm_df)  # rna_exp, voom=True)
-                results = results.to_dict(orient='records')
-        return results
+                matrix_name = args.schema + '_' + args.table
+                results = ge.pca_json(mm.get_norm_matrix(name=matrix_name))
+            except KeyError:
+                counts = pandas.read_sql_table(args.table,
+                                               index_col='Gene',
+                                               con=engine,
+                                               schema=args.schema)
+                mm.add_counts_matrix(name=matrix_name, counts_matrix=counts)
+                results = ge.pca_json(mm.get_norm_matrix(name=matrix_name))
+        return results.to_dict(orient='records')
 
 api.add_resource(pca_points, '/data')
