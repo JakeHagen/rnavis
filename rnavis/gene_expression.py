e6a735d9f34e49189df803f641eeac5241d0f3de
Jake Hagen <jake.hagen@opmbx.org>
2016-05-23 12:22:33 -0400

    add matrix_manipulation class, which abstracts common patterns away from the api and webapp. Allows webapp to not rely on standalone API without repeating a ton of code

--- rnavis/gene_expression.py
+++ rnavis/gene_expression.py
@@ -2,26 +2,7 @@ import rnavis.tmm as tmm
 import numpy as np
 import pandas
 from sklearn.decomposition import PCA as sklearnPCA
-
-"""
-Most likely will be removed as info can just be extracted from pandas DF
-
-class gene_expression:
-
-    '''number_of_groups is a estimate based on uniqueness of
-       non number characters in sample names
-    '''
-
-    def __init__(self, df):
-        self.df = df
-        self.matrix = df.as_matrix()
-        self.genes = df.index.values
-        self.samples = df.columns.values
-        self.lib_size = self.matrix.sum(axis=0)
-        self.number_of_groups = len(set(
-            [''.join([i for i in x if not i.isdigit()]) for x in self.samples]
-            ))
-"""
+from rnavis.combat import combat
 
 
 def counts_per_million(mat):
@@ -29,10 +10,19 @@ def counts_per_million(mat):
     return mat / (lib_size / 1e6)
 
 
-def rm_low_counts(mat, samples_per_group=None):
+def get_samples_per_group(df):
+    try:
+        samples = df.columns.values
+        number_of_groups = len(set([''.join([i for i in x if not i.isdigit()])
+                                    for x in samples]))
+        return int(len(samples) / number_of_groups)
+    except AttributeError:
+        return int(df.shape[1]/2)
+
+
+def rm_low_counts(mat):
     mat_cpm = counts_per_million(mat)
-    if not samples_per_group:
-        samples_per_group = int(mat.shape[1]/2)
+    samples_per_group = get_samples_per_group(mat)
     # remove genes that do not have at least 1 cpm in at n samples
     # where n is the number of samples in smallest group
     to_rm = (mat_cpm > 1).sum(axis=1) >= samples_per_group
@@ -40,14 +30,10 @@ def rm_low_counts(mat, samples_per_group=None):
 
 
 def norm_gene_matrix(mat):
-    try:
-        samples_per_group = mat.columns.values
-        mat = rm_low_counts(mat=mat, samples_per_group=samples_per_group)
-    except AttributeError:
-        mat = rm_low_counts(mat)
+    mat = rm_low_counts(mat=mat)
     lib_size = mat.sum(axis=0)
     norm_factors = (lib_size * tmm.calc_norm_factors(mat)) + 1
-    # mat_five = mat + .5
+    # mat = mat + .5
     return np.log2(mat / norm_factors * 1e6)
 
 
@@ -81,3 +67,60 @@ def pca_json(df, n_components=4, exp_var_min=.05):
                                    exp_var_list=exp_var,
                                    num_pc=num_pc)
     return pca_points_df
+
+
+class matrix_manipulation:
+
+    def __init__(self, counts_dict=None, norm_dict=None):
+        self.counts_dict = counts_dict or dict()
+        self.norm_dict = norm_dict or dict()
+        self.batch_dict = dict()
+
+    def add_counts_matrix(self, name, counts_matrix):
+        self.counts_dict[name] = counts_matrix
+
+    def get_counts_matrix(self, name):
+        return self.counts_dict[name]
+
+    def add_batch_matrix(self, name, batch_matrix):
+        self.batch_dict[name] = batch_matrix
+
+    def get_batch_matrix(self, name):
+        return self.batch_dict[name]
+
+    def add_norm_matrix(self, name, norm_matrix):
+        self.norm_dict[name] = norm_matrix
+
+    def get_norm_matrix(self, name):
+        try:
+            return self.norm_dict[name]
+        except KeyError:
+            self.norm_counts_matrix(name)
+            return self.norm_dict[name]
+
+    def norm_counts_matrix(self, name):
+        try:
+            self.norm_dict[name] = norm_gene_matrix(self.counts_dict[name])
+        except KeyError as e:
+            raise KeyError(
+                'need counts_matrix before we can normalize'
+                ).with_traceback(e.__traceback__)
+
+    def make_batch_matrix(self, batch_list, name=None, matrix=None):
+        if name and matrix is None:
+            try:
+                self.batch_dict[name] = combat(self.norm_dict[name],
+                                               batch=list(batch_list))
+            except KeyError:
+                try:
+                    self.norm_counts_matrix(name=name)
+                    self.batch_matrix = combat(self.norm_matrix,
+                                               batch=list(batch_list))
+                except KeyError as e:
+                    raise KeyError(
+                        'need a norm_matrix before it can be batch' +
+                        'corrected, also couldnt create norm matrix'
+                        ).with_traceback(e.__traceback__)
+        else:
+            corrected_matrix = combat(matrix, batch=list(batch_list))
+            return corrected_matrix
